bfcbb365610858f25c853563e6b75336f009ea1f
TFS-ACC <tabitha.stevens@g.austincc.edu>
2020-09-21 15:03:39 -0500

    hw2

--- src/classwork/03_assign/decision.cpp
+++ src/classwork/03_assign/decision.cpp
@@ -1,1 +1,86 @@
 //cpp
+#include "decision.h"
+
+//IF ELSE
+string get_letter_grade_using_if(int grade){
+    string if_grade;
+
+    if (grade >= 90 && grade < 100) {
+        if_grade = "A";
+    } else if (grade >= 80 && grade < 90) {
+        if_grade = "B";
+    } else if (grade >= 70 && grade < 80) {
+        if_grade = "C";
+    } else if (grade >= 60 && grade < 70) {
+        if_grade = "D";
+    } else if (grade <= 50 && grade > 0) {
+        if_grade = "F";
+    } else {
+        cout << "The number you have entered is out of range." << "\n";
+    }
+
+    return if_grade;
+}
+
+//SWITCH
+string get_letter_grade_using_switch(int grade){
+    string switch_grade;
+
+    switch (grade / 10) {
+        case(5):
+            switch_grade = "F";
+            break;
+        case(6):
+            switch_grade = "D";
+            break;
+        case(7):
+            switch_grade = "C";
+            break;
+        case(8):
+            switch_grade = "B";
+            break;
+        case(9):
+            switch_grade = "A";
+            break;
+        case(10):
+            switch_grade = "A";
+        default:
+            cout << "The number you have entered is out of range." << "\n";
+            break;
+    }
+
+    return switch_grade;
+}
+
+//SWITCH
+// string get_letter_grade_using_switch(int grade){
+//     string switch_grade;
+
+//     switch (grade / 10) {
+//         case(5):
+//             if (grade >= 90 && grade < 100)
+//             switch_grade = "A";
+//             break;
+//         case(2):
+//             if (grade >= 80 && grade < 90)
+//             switch_grade = "B";
+//             break;
+//         case(3):
+//             if (grade >= 70 && grade < 80)
+//             switch_grade = "C";
+//             break;
+//         case(4):
+//             if (grade >= 60 && grade < 70)
+//             switch_grade = "D";
+//             break;
+//         case(5):
+//             if (grade <= 50 && grade > 0)
+//             switch_grade = "F";
+//             break;
+//         default:
+//             cout << "The number you have entered is out of range." << "\n";
+//             break;
+//     }
+
+//     return switch_grade;
+// }
--- src/classwork/03_assign/main.cpp
+++ src/classwork/03_assign/main.cpp
@@ -1,9 +1,24 @@
 //Write the include statement for decisions.h here
-
+#include "decision.h"
 //Write namespace using statements for cout and cin
+using namespace std;
 
 int main() 
 {
+	int grade;
+
+	string if_letter, switch_letter;
+
+	cout << "Please enter your numeric grade:";
+
+	cin >> grade;
+
+	if_letter = get_letter_grade_using_if(grade);
+
+	switch_letter =  get_letter_grade_using_switch(grade);
+
+	cout << switch_letter << "\n";
+	cout << if_letter << "\n";
 
 	return 0;
 }
