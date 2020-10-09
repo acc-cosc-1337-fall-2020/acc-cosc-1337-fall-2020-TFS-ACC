a3ca16accbeb18d856873713bbcdb89125d97e5c
TFS-ACC <tabitha.stevens@g.austincc.edu>
2020-10-08 22:02:13 -0500

    complete

--- src/homework/04_iteration/dna.cpp
+++ src/homework/04_iteration/dna.cpp
@@ -7,15 +7,43 @@ Iterate string count Gs and Cs, divide count by string length.
 Return quotient.
 */
 
+double get_gc_content(const string &dna){
 
+    int gccount = 0;
+    double gccontent = 0;
 
+    for (string::size_type i = 0; i < dna.length(); i++){
+        
+        if(dna[i] == 'G' || dna[i] == 'C'){
+            gccount++;
+        }
+
+    }
+
+    gccontent = (double)gccount / dna.length();
+    
+    return gccontent;
+
+}
 
 /*
 Write code for function get_reverse_string that
 accepts a string parameter and returns a string reversed.
 */
 
+string get_reverse_string(string dna){
 
+    string reversedDna;
+
+    for (int i = dna.length()-1; i >= 0; i--){
+
+        reversedDna.push_back(dna[i]);
+
+    }
+
+    return reversedDna;
+
+}
 
 /*
 Write prototype for function get_dna_complement that
@@ -28,3 +56,35 @@ c. return string
 
 */
 
+string get_dna_complement(string dna){
+
+    string reversedDna = get_reverse_string(dna);
+
+    int slength = dna.length();
+
+    for (int i = 0; i < slength; i++){
+        
+        if(reversedDna[i] == 'A'){
+
+            reversedDna[i] = 'T';
+
+        } else if (reversedDna[i] == 'T'){
+
+            reversedDna[i] = 'A';
+
+        } else if (reversedDna[i] == 'C'){
+
+            reversedDna[i] = 'G';
+
+        } else if (reversedDna[i] == 'G'){
+
+            reversedDna[i] = 'C';
+
+        }
+
+
+    }
+
+    return reversedDna;
+
+}
--- src/homework/04_iteration/main.cpp
+++ src/homework/04_iteration/main.cpp
@@ -1,8 +1,7 @@
 //write include statements
-
+#include "dna.h"
 //write using statements
-
-
+using namespace std;
 /*
 Write code that prompts user to enter 1 for Get GC Content, 
 or 2 for Get DNA Complement.  The program will prompt user for a 
@@ -12,5 +11,55 @@ user enters a y or Y.
 */
 int main() 
 {
+	char cont = 'Y';
+
+
+	do {
+
+		int choice;
+		int choicevalid = 0;
+		string dnastring;
+		string complement;
+		double content;
+
+		while (choicevalid == 0){
+
+			cout << "enter 1 for Get GC Content, or 2 for Get DNA Complement.: ";
+			cin >> choice;
+
+			if (choice == 1 || choice == 2) {
+				choicevalid = 1;
+			}
+		
+
+		}
+
+		if (choice == 1){
+
+			cout << "GC content chosen." << "\n" << "Please enter your DNA string.: ";
+			cin >> dnastring;
+
+			content = get_gc_content(dnastring);
+
+			cout << "The GC content of this string is: " << content << "\n";
+
+		} else if (choice == 2){
+
+			cout << "DNA complement chosen." << "\n" << "Please enter your DNA string.: ";
+			cin >> dnastring;
+
+			complement = get_dna_complement(dnastring);
+
+			cout << "The DNA complement of this string is: " << complement << "\n";
+
+		}
+
+
+		cout << "Would you like to evaluate another string? (Y/n): ";
+		cin >> cont;
+
+	} while (cont == 'y' || cont == 'Y');
+
+
 	return 0;
 }
